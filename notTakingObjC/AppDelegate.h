de17048012ec566da7cfe9b995d5693f164b4ca6
kimbui <kim.bui@allegion.com>
2015-08-27 14:38:22 -0600

    Initial Commit

---
+++ notTakingObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  notTakingObjC
+//
+//  Created by Kim Bui on 8/27/15.
+//  Copyright (c) 2015 Kim Bui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ notTakingObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  notTakingObjC
+//
+//  Created by Kim Bui on 8/27/15.
+//  Copyright (c) 2015 Kim Bui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
